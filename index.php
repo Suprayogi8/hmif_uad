b07433a68e9d4860277fe8953006acb810bc1353
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-07-03 16:49:46 +0700

    Hapus penggunaan node js

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="output.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap');
+        * {
+            font-family: 'poppins';
+        }
+    </style>
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <nav class="w-full h-28 border ">
+        <div class="flex justify-between h-full px-5 items-center text-xl">
+            <ul class="flex space-x-5">
+                <div class="logo"></div>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Bank Soal</a></li>
+                <li><a href="#">Alumni</a></li>
+                <li><a href="#">Aspirasi</a></li>
+                <li><a href="#">Informatic Store</a></li>
+                <li><a href="#">Tentang Kami</a></li>
+            </ul>
+            <a href="">Login</a>
+        </div>
+    </nav>
 </body>
 </html>
