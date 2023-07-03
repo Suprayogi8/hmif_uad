5620d72f4bf45c11f8fbadc81327ffef23d12d7b
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-07-03 20:24:31 +0700

    add demisioner, alumni, bank soal

---
+++ bank_soal.php
@@ -0,0 +1,46 @@
+<?php
+
+include "koneksi.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap');
+
+        * {
+            font-family: 'poppins';
+        }
+    </style>
+</head>
+
+<body class="bg-gray-200">
+    <nav class="w-full h-28 shadow-lg fixed z-10 bg-white">
+        <div class="flex justify-between h-full px-10 items-center text-xl">
+            <ul class="flex space-x-5">
+                <div class="logo"></div>
+                <li class="font-bold"><a href="#">Home</a></li>
+                <li><a href="#">Bank Soal</a></li>
+                <li><a href="#">Alumni</a></li>
+                <li><a href="#">Aspirasi</a></li>
+                <li><a href="#">Informatic Store</a></li>
+                <li><a href="#">Tentang Kami</a></li>
+            </ul>
+            <a href="">Login</a>
+        </div>
+    </nav>
+
+    <section class="w-full py-36 max-h-fit">
+        
+    </section>
+
+
+</body>
+
+</html>
