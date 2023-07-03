d63cec5de8d96553cbf238ced961fff9a266d26c
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-07-03 21:06:34 +0700

    Menambahkan file demisioner hmif

--- demis.php
+++ demis.php
@@ -57,33 +57,39 @@ if(isset($_POST['submit'])){
         <div class="w-[85%] mx-auto max-h-fit py-10 flex flex-col bg-white items-center">
             <p>Demisioner HMIF</p>
             <div class="space-y-10 mx-auto w-[80%] mt-32">
-                <div class="card-demis w-full border shadow-md hover:shadow-lg">
-                    <p class="w-full py-4 px-2 bg-gray-200">Periode 2021</p>
-                    <div class="relative h-[500px]">
-                        <img src="" alt="belum">
-                        <button class="absolute right-5 bottom-5 py-2 px-3 bg-gray-400">
-                            Selengkapnya
-                        </button>
-                    </div>
-                </div>
-                <div class="card-demis w-full border shadow-md hover:shadow-lg">
-                    <p class="w-full py-4 px-2 bg-gray-200">Periode 2021</p>
-                    <div class="relative h-[500px]">
-                        <img src="" alt="belum">
-                        <button class="absolute right-5 bottom-5 py-2 px-3 bg-gray-400">
-                            Selengkapnya
-                        </button>
-                    </div>
-                </div>
-                <div class="card-demis w-full border shadow-md hover:shadow-lg">
-                    <p class="w-full py-4 px-2 bg-gray-200">Periode 2021</p>
-                    <div class="relative h-[500px]">
-                        <img src="" alt="belum">
-                        <button class="absolute right-5 bottom-5 py-2 px-3 bg-gray-400">
-                            Selengkapnya
-                        </button>
-                    </div>
-                </div>
+                <?php 
+                
+                $sql = "SELECT * FROM demis";
+                $query = mysqli_query($koneksi, $sql);
+                if($query){
+                    if($query->num_rows > 0){
+
+                        while($row = mysqli_fetch_array($query)){
+                            
+                            ?>
+                            
+                            <div class="card-demis w-full border shadow-md hover:shadow-lg">
+                                <p class="w-full py-4 px-2 bg-gray-200"><?=$row['periode']?></p>
+                                <div class="relative h-[500px]">
+                                    <img src="<?=$row['gambar']?>" class="w-full h-full bg-cover" alt="belum">
+                                    <button class="absolute right-5 bottom-5 py-2 px-3 bg-gray-400">
+                                        Selengkapnya
+                                    </button>
+                                </div>
+                            </div>
+
+                            <?php
+
+                        }
+
+                    }else{
+                        echo '<p class="px-32 py-4 bg-yellow-400">Belum ada data!</p>';
+                    }
+                }else{
+                    echo '<p class="px-32 py-4 bg-yellow-400">Gagal memuat data!</p>';
+                }
+
+                ?>                
             </div>
         </div>
     </section>
