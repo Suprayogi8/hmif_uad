936ad95e195a26f69b2999f0c72d9459a263d709
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-07-03 17:37:52 +0700

    Halaman Berita

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<?php
+
+include "koneksi.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,17 +13,19 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap');
+
         * {
             font-family: 'poppins';
         }
     </style>
 </head>
-<body>
-    <nav class="w-full h-28 border ">
-        <div class="flex justify-between h-full px-5 items-center text-xl">
+
+<body class="bg-gray-200">
+    <nav class="w-full h-28 shadow-lg fixed z-10 bg-white">
+        <div class="flex justify-between h-full px-10 items-center text-xl">
             <ul class="flex space-x-5">
                 <div class="logo"></div>
-                <li><a href="#">Home</a></li>
+                <li class="font-bold"><a href="#">Home</a></li>
                 <li><a href="#">Bank Soal</a></li>
                 <li><a href="#">Alumni</a></li>
                 <li><a href="#">Aspirasi</a></li>
@@ -27,5 +35,50 @@
             <a href="">Login</a>
         </div>
     </nav>
+
+    <section class="w-full py-36">
+        <div class="w-[90%] h-20 border mx-auto">
+            <!--  PHP Untuk memanggil data news pada database  -->
+            <?php
+
+            $sql = "SELECT * FROM berita";
+            $index = 0;
+            $query = mysqli_query($koneksi, $sql);
+            if ($query->num_rows > 0) {
+                while ($row = mysqli_fetch_array($query)) {
+                    if ($index == 0) {
+            ?>
+
+                        <form class="w-full bg-white relative h-[400px]">
+                            <input type="hidden" value="<?= $row['id_berita'] ?>" name="id">
+                            <img src="<?= $row['gambar'] ?>" class="w-full h-full bg-cover" alt="News">
+                            <div class="absolute top-0 left-0 text-white max-w-fit px-10 bg-gradient-to-br from-black to-transparent">
+                                <p class="text-2xl max-w-[90%] py-2"><?= $row['judul'] ?></p>
+                            </div>
+                            <div class="absolute max-w-fit left-10 bottom-7 flex space-x-5">
+                                <a href="" class="px-3 py-2 bg-gray-300">Komen</a>
+                                <a href="#" class="px-3 py-2 bg-gray-300">Selengkapnya</a>
+                            </div>
+
+                        </form>
+
+                    <?php
+                    } else {
+                    ?>
+
+                        
+
+                    <?php
+                    }
+                    $index++;
+                }
+            }
+
+            ?>
+
+
+        </div>
+    </section>
 </body>
+
 </html>
---
+++ koneksi.php
@@ -0,0 +1,3 @@
+<?php
+
+$koneksi = mysqli_connect("localhost", "root", "", "hmif_uad");
